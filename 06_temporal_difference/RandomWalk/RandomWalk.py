b415b37ab1af23ce57d3ca88cc95e8569cce726f
Paulo Bruno <pbrunosousa@gmail.com>
2019-10-20 23:03:06 -0300

    Implement RandomWalk as Sutton and Barto's eg 6.2

--- 06_temporal_difference/RandomWalk/RandomWalk.py
+++ 06_temporal_difference/RandomWalk/RandomWalk.py
@@ -3,18 +3,12 @@ import numpy as np
 from gym.utils import seeding
 from gym import error, spaces
 
-r_L = 0
-r_R = 1
-
-gamma = 0.9
-
 class RandomWalk:
-    def __init__(self, width=5, height=5):
+    def __init__(self, states=5):
         self.__version__ = "0.0.1"
-        self.width = width
-        self.height = height
+        self.states = states
 
-        self._action_set = [0, 1, 2, 3]
+        self._action_set = [0, 1]
         self.action_space = spaces.Discrete(len(self._action_set))
 
         self.seed()
@@ -25,77 +19,42 @@ class RandomWalk:
         return [seed]
 
     def reset(self):
-        self._values = np.zeros((self.width, self.height))
-        self.state = [self.np_random.randint(self.width), 
-                      self.np_random.randint(self.height)]
-        return np.array(self.state)
+        # define next state table
+        self.nextState = np.zeros((2, self.states), dtype=int) # two actions, n states
+        self.nextState[0] = [(i-1) for i in range(self.states)]
+        self.nextState[1] = [(i+1) for i in range(self.states)]
+        self.nextState[0,0] = -1
+        self.nextState[1,self.states-1] = -1
+
+        # define reward table
+        self.reward = np.zeros((2, self.states), dtype=int)
+        self.reward[1, self.states-1] = 1
+
+        # start is always the center state
+        self.current_state = int(self.states / 2)
+
+        return self.current_state
         
     def step(self, action):
-        done = False
-        reward = 0
-
-        if action == 0: # up
-            if self.state[0] == 0:
-                reward = -1
-            elif self.state == [A[0]+1, A[1]]:
-                reward = r_A
-                self.state = list(A_n)
-            elif self.state == [B[0]+1, B[1]]:
-                reward = r_B
-                self.state = list(B_n)
-            else:
-                self.state[0] = self.state[0] - 1
-        elif action == 1: # down
-            if self.state[0] == self.height-1:
-                reward = -1
-            elif self.state == [A[0]-1, A[1]]:
-                reward = r_A
-                self.state = list(A_n)
-            elif self.state == [B[0]-1, B[1]]:
-                reward = r_B
-                self.state = list(B_n)
-            else:
-                self.state[0] = self.state[0] + 1
-        elif action == 2: # left
-            if self.state[1] == 0:
-                reward = -1
-            elif self.state == [A[0], A[1]+1]:
-                reward = r_A
-                self.state = list(A_n)
-            elif self.state == [B[0], B[1]+1]:
-                reward = r_B
-                self.state = list(B_n)
-            else:
-                self.state[1] = self.state[1] - 1
-        elif action == 3: # right
-            if self.state[1] == self.width-1:
-                reward = -1
-            elif self.state == [A[0], A[1]-1]:
-                reward = r_A
-                self.state = list(A_n)
-            elif self.state == [B[0], B[1]-1]:
-                reward = r_B
-                self.state = list(B_n)
-            else:
-                self.state[1] = self.state[1] + 1
-        else:
+        if (action != 0) and (action != 1):
             raise error.Error('Invalid action: ' + str(action))
-            
-        return np.array(self.state), reward, done, {}
+
+        old_state = self.current_state
+        self.current_state = self.nextState[action, old_state]
+        r = self.reward[action, old_state]
+        done = True if self.current_state == -1 else False # -1 is the terminal state
+
+        return self.current_state, r, done, {}
         
     # TODO: melhorar essa maneira de desenhar o grid, ver exemplo taxi
     def render(self):
-        for i in range(self.height):
-            print('+', end='')
-            for j in range(self.width):
-                print("-------+", end='')
-            print('\n', end='|')
-            for j in range(self.width):
-                print("{:6.2f}".format(self._values[i, j]) + ' ', end='|')
-            print()
-        print('+', end='')
-        for j in range(self.width):
-            print("-------+", end='')
+        for i in range(self.states):
+            if i == self.current_state:
+                print(">", end="")
+            print(str(i), end="")
+            if i == self.current_state:
+                print("<", end="")
+            print(" ", end="")
         print()
 
     def close(self):
@@ -104,15 +63,25 @@ class RandomWalk:
     def get_action_meanings(self):
         return [ACTION_MEANING[i] for i in self._action_set]
 
-    def _reward(self, state, action):
-        if state == A:
-            return r_A
-        elif state == B:
-            return r_B
-        else:
-            return 0
-
 ACTION_MEANING = {
     0: "LEFT",
     1: "RIGHT"
 }
+
+
+env = RandomWalk()
+
+observation = env.reset()
+
+for _ in range(10):
+  env.render()
+  action = env.action_space.sample()
+  print("action: " + env.get_action_meanings()[action])
+  observation, reward, done, info = env.step(action)
+
+  if done:
+    observation = env.reset()
+    print("r: " + str(reward))
+    print()
+    
+env.close()
